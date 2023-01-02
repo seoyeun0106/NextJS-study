72c630bcdcc0d865434124f77a0b96af715c3655
seoyeun0106 <sarah.mun16@gmail.com>
2023-01-03 01:41:15 +0900

    Feat: SSR 조절하기

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,36 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 import axios from "axios";
-export default function Home() {
+
+export default function Home({ results }) {
   const [movies, setMovies] = useState([]);
 
-  useEffect(() => {
-    const getAPI = async () => {
-      const result = await axios.get("/movies");
-      console.log(result);
-      setMovies(result.data.results);
-    };
-    getAPI();
-  }, []);
+  // useEffect(() => {
+  //   const getAPI = async () => {
+  //     const result = await axios.get("/movies");
+  //     console.log(result);
+  //     setMovies(result.data.results);
+  //   };
+  //   getAPI();
+  //    setMovies(result.data.results);
+  // }, []);
   return (
     <>
       <Seo title="seoyeun"></Seo>
       <h2>Popular Movies</h2>
-      {movies.map((movie) => {
+      {results.map((movie) => {
         return <div>{movie.original_title}</div>;
       })}
     </>
   );
+}
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch("http://localhost:3000/movies")
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
